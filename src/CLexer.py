c40096e6e4221410a24673ecdf7072a4dac40be2
Prodrik <adrian.munnoz.lopez04@gmail.com>
2024-10-08 11:08:16 +0200

    Revert "Update CLexer.py"
    This reverts commit 2f5c1e52907fdacdf981bbec9a6072f84c93f35e.

--- src/CLexer.py
+++ src/CLexer.py
@@ -1,21 +1,5 @@
 from sly import Lexer
 
-
-
-def leer_fichero(nombre_fichero):
-    try:
-        with open(nombre_fichero, 'r', encoding='utf-8') as fichero:
-            data = fichero.read()  # Lee todo el contenido del archivo
-    except FileNotFoundError:
-        print(f"El fichero {nombre_fichero} no existe.")
-        data = None
-    except Exception as e:
-        print(f"Ocurrió un error al leer el archivo: {e}")
-        data = None
-    return data
-
-
-
 class CLexer(Lexer):
     tokens = { ID, INT, NUMBER }
     ignore = ' \t'
