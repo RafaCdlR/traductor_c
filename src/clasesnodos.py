bb9025ef8a1ca795548f70ed411de921f2b1b201
Alejandro García Ramos <99274836+maroyo@users.noreply.github.com>
2024-11-04 20:21:18 +0100

    cuidaITO EN Q NO ESTA TERMINAO

---
+++ src/clasesnodos.py
@@ -0,0 +1,128 @@
+class Nodo:
+    
+    def cadena(self):
+        return ""
+    
+    def escribe(self):
+        pass
+
+    def __str__(self):
+        return self.cadena()
+
+
+
+class Nodotermino(Nodo):
+    valor = None #valor numerico
+    def __init__(self, v):
+        self.valor = v
+
+
+    def cadena(self):
+        return f"{self.valor}" #devuelve cadena a imprimir
+
+    def escribe(self):
+        print(self.cadena())
+
+        
+
+    
+
+
+class Nodosumaresta(Nodo):
+    left = ""
+    right = ""
+    operador = ""
+
+    def __init__(self , left  , right =""  , operador ="" ):
+        assert( operador == '+' or operador == '-' or operador == "" )
+        self.left = left
+        self.right = right
+        self.operador = operador
+       
+
+    def cadena(self):
+        return f"{self.left.cadena()}{self.operador}{self.right.cadena()}" 
+    
+    def escribe(self):
+        print(self.cadena())
+
+class NodoMultDiv(Nodo):
+    left = ""
+    right = ""
+    operador = ""
+
+    def __init__(self , left  , right =""  , operador ="" ):
+        assert( operador == '*' or operador == '/' or operador == "" )
+        self.left = left
+        self.right = right
+        self.operador = operador
+       
+
+    def cadena(self):
+        return f"{self.left.cadena()}{self.operador}{self.right.cadena()}" 
+    
+    def escribe(self):
+        print(self.cadena())
+
+
+
+
+class NodoopUn(Nodo):
+    operador = ""
+    def __init__(self,operador):
+        assert(operador == "-" or operador == "!")
+        self.operador = operador
+
+    def cadena(self):
+        return f"{self.operador}"
+    
+    def escribe(self):
+        print(self.cadena())
+
+
+class NodoopUnario(Nodo):
+    operador = ""
+    right = ""
+
+    def __init__(self,operador,right):
+        
+        self.operador = operador
+        self.right = right
+
+    def cadena(self):
+        return f"{self.operador.cadena()}{self.right.cadena()}"
+    
+    def escribe(self):
+        print(self.cadena())
+
+
+class NodoopLogAnd(Nodo):
+    operador = "&&"
+    right = ""
+
+    def __init__(self,right):
+        
+        
+        self.right = right
+
+    def cadena(self):
+        return f"{self.operador.cadena()}{self.right.cadena()}"
+    
+    def escribe(self):
+        print(self.cadena())
+
+
+
+
+class NodoOpComp(Nodo):
+
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def cadena(self):
+        return f"{self.left.cadena()}{self.op}{self.right.cadena()}"
+
+    def escribe(self):
+        print(self.cadena())
